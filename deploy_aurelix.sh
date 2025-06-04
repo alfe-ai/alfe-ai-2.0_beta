98d37120db7426e6cc62262d6ae96ffaa23db80c
Lochner <nlochner@alfe.sh>
2025-06-03 21:12:32 -0500

    Create deploy_aurelix.sh

---
+++ deploy_aurelix.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+rm -rf alfe-ai-Aurelix
+git clone https://github.com/alfe-ai/alfe-ai-Aurelix.git
+cd alfe-ai-Aurelix
+git checkout Aurora/Aurelix/dev/main-rel2
+cp ../.env .env
+./run_full.sh
