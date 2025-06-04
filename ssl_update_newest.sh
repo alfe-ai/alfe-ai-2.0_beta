1b4815b082b30213adf97a03204d56605680395e
Lochner <nlochner@alfe.sh>
2025-06-04 15:23:31 -0500

    Update ssl_update_newest.sh

--- ssl_update_newest.sh
+++ ssl_update_newest.sh
@@ -5,3 +5,9 @@ sudo readlink -f /etc/letsencrypt/live/mvp2.alfe.sh/privkey.pem
 CERT=$(sudo readlink -f /etc/letsencrypt/archive/mvp2.alfe.sh/privkey1.pem)
 sudo chgrp ssl-cert "$CERT"
 sudo chmod 640 "$CERT"
+
+admin@ip-172-26-6-245:~/alfe-ai-Aurelix$ sudo chmod +x /etc/letsencrypt/
+admin@ip-172-26-6-245:~/alfe-ai-Aurelix$ sudo chmod +x /etc/letsencrypt/archive/
+admin@ip-172-26-6-245:~/alfe-ai-Aurelix$ sudo chmod +x /etc/letsencrypt/archive/mvp2.alfe.sh/
+admin@ip-172-26-6-245:~/alfe-ai-Aurelix$ sudo chmod 644 /etc/letsencrypt/archive/mvp2.alfe.sh/privkey1.pem
+admin@ip-172-26-6-245:~/alfe-ai-Aurelix$ ls /etc/letsencrypt/archive/mvp2.alfe.sh/privkey1.pem
