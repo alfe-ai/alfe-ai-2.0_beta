de074293a09679676e3d1f94e2f911f281a03ccd
Lochner <nlochner@alfe.sh>
2025-06-03 21:39:35 -0500

    Update deploy_aurelix.sh

--- deploy_aurelix.sh
+++ deploy_aurelix.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install git nodejs npm
+sudo apt install git nodejs npm -y
 rm -rf alfe-ai-Aurelix
 git clone https://github.com/alfe-ai/alfe-ai-Aurelix.git
 cd alfe-ai-Aurelix
