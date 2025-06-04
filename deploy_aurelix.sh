42eea53dfe0a3b17a56aa2bd47b568c0fbbf07bb
Lochner <nlochner@alfe.sh>
2025-06-03 21:14:57 -0500

    Update deploy_aurelix.sh

--- deploy_aurelix.sh
+++ deploy_aurelix.sh
@@ -4,5 +4,5 @@ rm -rf alfe-ai-Aurelix
 git clone https://github.com/alfe-ai/alfe-ai-Aurelix.git
 cd alfe-ai-Aurelix
 git checkout Aurora/Aurelix/dev/main-rel2
-cp ../.env .env
+cp ../.env Aurora/.env
 ./run_full.sh
