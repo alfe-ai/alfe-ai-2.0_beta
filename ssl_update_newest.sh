9e75d564a91639219280a26740a31c36752a3791
Lochner <nlochner@alfe.sh>
2025-06-04 15:27:48 -0500

    Update ssl_update_newest.sh

--- ssl_update_newest.sh
+++ ssl_update_newest.sh
@@ -11,3 +11,14 @@ admin@ip-172-26-6-245:~/alfe-ai-Aurelix$ sudo chmod +x /etc/letsencrypt/archive/
 admin@ip-172-26-6-245:~/alfe-ai-Aurelix$ sudo chmod +x /etc/letsencrypt/archive/mvp2.alfe.sh/
 admin@ip-172-26-6-245:~/alfe-ai-Aurelix$ sudo chmod 644 /etc/letsencrypt/archive/mvp2.alfe.sh/privkey1.pem
 admin@ip-172-26-6-245:~/alfe-ai-Aurelix$ ls /etc/letsencrypt/archive/mvp2.alfe.sh/privkey1.pem
+
+admin@ip-172-26-6-245:~/alfe-ai-Aurelix$ sudo chmod +x /etc/letsencrypt/live
+admin@ip-172-26-6-245:~/alfe-ai-Aurelix$ sudo chmod +x /etc/letsencrypt/live/mvp2.alfe.sh
+admin@ip-172-26-6-245:~/alfe-ai-Aurelix$ sudo chmod +x /etc/letsencrypt/live/mvp2.alfe.sh/privkey.pem
+admin@ip-172-26-6-245:~/alfe-ai-Aurelix$ cat .env
+cat: .env: No such file or directory
+admin@ip-172-26-6-245:~/alfe-ai-Aurelix$ 
+admin@ip-172-26-6-245:~/alfe-ai-Aurelix$ chmod +x /etc/letsencrypt/live/mvp2.alfe.sh/fullchain.pem
+chmod: changing permissions of '/etc/letsencrypt/live/mvp2.alfe.sh/fullchain.pem': Operation not permitted
+admin@ip-172-26-6-245:~/alfe-ai-Aurelix$ sudo chmod +x /etc/letsencrypt/live/mvp2.alfe.sh/fullchain.pem
+admin@ip-172-26-6-245:~/alfe-ai-Aurelix$ 
